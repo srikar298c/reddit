7f5d33c1691c3543fc29b093c72218cb5dcf3f9e
sri-gudakesa2703 <srikar2985969c@gmail.com>
2023-02-07 20:09:22 +0530

    Layout and Navbar

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Layout:React.FC = ({}) => {
+    
+    return(
+        <>
+        <Navbar/>
+       <main>{}</main>
+        </>
+
+    )}
+export default Layout;

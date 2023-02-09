d737380bceb765c9cf6fd1b1c0f52df5a1ef9c69
sri-gudakesa2703 <srikar2985969c@gmail.com>
2023-02-10 03:31:00 +0530

    Auth modal

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -3,17 +3,25 @@ import React from 'react';
 
 
 
-const AuthButtons:React.FC= () => {
-    
+const AuthButtons: React.FC = () => {
+
     return (
         <>
-        <Button variant={'outline'}
-        height="20px" display ={{base:"none", sm:"flex"}}
-        width={{base:"70px", md:"110px"}}
-        mr={2}
-        // onClick={()=>{}}
-        >LogIn</Button>
-        <Button variant={'solid'}>SignUp</Button>
+            <Button variant={'outline'}
+                height="28px"
+                display={{ base: "none", sm: "flex" }}
+                width={{ base: "70px", md: "110px" }}
+                mr={2}
+            // onClick={()=>{}}
+            >LogIn</Button>
+
+            <Button
+                variant={'solid'}
+                height="28px"
+                display={{ base: "none", sm: "flex" }}
+                width={{ base: "70px", md: "110px" }}
+                mr={2}
+            >SignUp</Button>
         </>
     )
 }

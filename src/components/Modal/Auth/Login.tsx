2b8651f17aa8a27c6369e46524f7880266a6533e
srikar kudurmalla <91621052+srikar298c@users.noreply.github.com>
2023-06-20 21:50:53 +0530

    working on login(3)
    Took 8 minutes

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -1,19 +1,38 @@
-import React, {useState} from "react";
-import {Input} from "@chakra-ui/react";
-type LoginProps ={
+import React, { useState } from "react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+// import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { ModalView } from "@/atoms/authModalAtom";
+// import { auth } from "../../../firebase/clientApp";
+// import { FIREBASE_ERRORS } from "../../../firebase/errors";
+import InputItem from "../../Layout/InputItem";
+// import {RecoilLoadable} from "recoil";
+// import loading = RecoilLoadable.loading;
 
-}
-const Login: React.FC<LoginProps> =() =>{
+type LoginProps = {
+    toggleView: (view: ModalView) => void;
+};
+
+const Login: React.FC<LoginProps> = ({ toggleView }) => {
     const [form, setForm] = useState({
         email: "",
         password: "",
     });
+    const [formError, setFormError] = useState("");
+    
+    // const [signInWithEmailAndPassword, _, loading, authError] =
+    //     useSignInWithEmailAndPassword(auth);
+    
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (formError) setFormError("");
         if (!form.email.includes("@")) {
             return setFormError("Please enter a valid email");
-    }
+        }
+        
+        // Valid form inputs
+        // signInWithEmailAndPassword(form.email, form.password);
+    };
+    
     const onChange = ({
                           target: { name, value },
                       }: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,23 +42,60 @@ const Login: React.FC<LoginProps> =() =>{
         }));
     };
     
-    return(
+    return (
         <form onSubmit={onSubmit}>
-            <Input
+            <InputItem
                 name="email"
                 placeholder="email"
                 type="text"
                 mb={2}
-                 onChange={onChange}
+                onChange={onChange}
             />
-            <Input
+            <InputItem
                 name="password"
                 placeholder="password"
                 type="password"
-                 onChange={onChange}
+                onChange={onChange}
             />
+            {/*<Text textAlign="center" mt={2} fontSize="10pt" color="red">*/}
+            {/*    {formError ||*/}
+            {/*        FIREBASE_ERRORS[authError?.message as keyof typeof FIREBASE_ERRORS]}*/}
+            {/*</Text>*/}
+            <Button
+                width="100%"
+                height="36px"
+                mb={2}
+                mt={2}
+                type="submit"
+                // isLoading={loading}
+            >
+                Log In
+            </Button>
+            <Flex justifyContent="center" mb={2}>
+                <Text fontSize="9pt" mr={1}>
+                    Forgot your password?
+                </Text>
+                <Text
+                    fontSize="9pt"
+                    color="blue.500"
+                    cursor="pointer"
+                    onClick={() => toggleView("resetPassword")}
+                >
+                    Reset
+                </Text>
+            </Flex>
+            <Flex fontSize="9pt" justifyContent="center">
+                <Text mr={1}>New here?</Text>
+                <Text
+                    color="blue.500"
+                    fontWeight={700}
+                    cursor="pointer"
+                    onClick={() => toggleView("signup")}
+                >
+                    SIGN UP
+                </Text>
+            </Flex>
         </form>
-    )
-}
+    );
+};
 export default Login;
-

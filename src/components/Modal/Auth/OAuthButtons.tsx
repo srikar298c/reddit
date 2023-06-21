b8abc17726f7e2d3d9143975a4351176c175d358
srikar kudurmalla <91621052+srikar298c@users.noreply.github.com>
2023-06-21 15:53:05 +0530

    working on oauth
    Took 6 minutes

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import {Button, Flex} from "@chakra-ui/react";
+import {Button, Flex, Image, Text } from "@chakra-ui/react";
 const OAuthButtons: React.FC =()=>{
     return(
-        <Flex>
-            <Button>Continue with Google</Button>
+        <Flex direction="column" mb={4} width="100%">
+            <Button
+                variant="oauth"
+                mb={2}
+                // onClick={() => signInWithGoogle()}
+                // isLoading={loading}
+            >
+                <Image src="/images/googlelogo.png" height="20px" mr={4} />
+                Continue with Google
+            </Button>
+            <Button variant="oauth">Some Other Provider</Button>
+            
         </Flex>
     )
 }

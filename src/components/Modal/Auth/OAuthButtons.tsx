497368ec704bd75304cf5397b667901a76ebbf9a
srikar kudurmalla <91621052+srikar298c@users.noreply.github.com>
2023-06-25 11:42:53 +0530

    Google signin
    Took 16 minutes

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,22 +1,23 @@
 import React from "react";
-import {Button, Flex, Image, Text } from "@chakra-ui/react";
+import {Button, Flex, Image, Text} from "@chakra-ui/react";
 import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
 import {auth} from "@/firebase/clientApp";
 
 const OAuthButtons: React.FC =()=>{
+    const [signInWithGoogle, user,loading,error] = useSignInWithGoogle(auth)
     return(
         <Flex direction="column" mb={4} width="100%">
             <Button
                 variant="oauth"
                 mb={2}
-                // onClick={() => signInWithGoogle()}
-                // isLoading={loading}
+                 onClick={() => signInWithGoogle()}
+                 isLoading={loading}
             >
                 <Image src="/images/googlelogo.png" height="20px" mr={4} />
                 Continue with Google
             </Button>
             <Button variant="oauth">Some Other Provider</Button>
-            
+            {error && <Text>{error.message}</Text>}
         </Flex>
     )
 }

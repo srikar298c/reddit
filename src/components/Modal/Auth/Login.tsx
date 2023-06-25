a5fb89d7f394636ab6f3aeeb64edd2f78d9d50ed
srikar kudurmalla <91621052+srikar298c@users.noreply.github.com>
2023-06-25 12:13:36 +0530

    Authentication completed
    Took 23 minutes

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -3,11 +3,14 @@ import { Button, Flex, Text } from "@chakra-ui/react";
 import {authModalState, } from "@/atoms/authModalAtom";
 import InputItem from "../../Layout/InputItem";
 import {useSetRecoilState} from "recoil";
+import {auth} from '@/firebase/clientApp'
+import {useSignInWithEmailAndPassword} from "react-firebase-hooks/auth";
+import {FIREBASE_ERRORS} from "@/firebase/error";
 
 type LoginProps = {
 
 };
-
+const [signInWithEmailAndPassword,user,loading, error ] = useSignInWithEmailAndPassword(auth)
 const Login: React.FC<LoginProps> = () => {
     const setAuthModalState = useSetRecoilState(authModalState);
     const [loginForm, setLoginForm] = useState({
@@ -16,11 +19,10 @@ const Login: React.FC<LoginProps> = () => {
     });
     
     
-    const onSubmit = () => {
-    
-    
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
         
-       
+        signInWithEmailAndPassword(loginForm.email, loginForm.password);
     };
     
     const onChange = (event:React.ChangeEvent<HTMLInputElement>)=>{
@@ -46,14 +48,19 @@ const Login: React.FC<LoginProps> = () => {
                 type="password"
                 onChange={onChange}
             />
-           
+            <Text
+                align={"center"}
+                color={"red"}
+                fontSize ="10pt"
+            >{ FIREBASE_ERRORS[error?.message as keyof typeof
+                FIREBASE_ERRORS]}</Text>
             <Button
                 width="100%"
                 height="36px"
                 mb={2}
                 mt={2}
                 type="submit"
-              
+              isLoading={loading}
             >
                 Log In
             </Button>
